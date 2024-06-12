d8d86cf96a42ddb447e6b70265683e6cfe6680de
Kushal Niroula <niroulakushal31@gmail.com>
2024-06-12 07:39:15 +0545

    feat: add user view and update

--- main.c
+++ main.c
@@ -1,13 +1,77 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void PerformAdd()
+struct User
 {
-    printf("Input first: ");
-    printf("Input first: ");
-    printf("Input first: ");
-    printf("Input first: ");
-    printf("Input first: ");
+    char username[500];
+    char password[500];
+};
+
+
+void ViewUserList()
+{
+    FILE *fp;
+    struct User u;
+    fp = fopen("user.dat", "rb");
+
+    system("cls");
+
+    printf("User List \n");
+    printf("Username \t Password \n");
+    printf("___________________________ \n");
+    while(
+          fread(&u, sizeof(u), 1, fp) == 1
+          )
+    {
+        printf("%s \t %s \n", u.username, u.password);
+    }
+    fclose(fp);
+
+    printf("Press any key to continue..");
+    getch();
+}
+
+void UpdateUser()
+{
+    FILE *fp;
+    FILE *tmp;
+    struct User input, fromFile, toTempFile;
+    system("cls");
+    printf("Please enter the username and password \n");
+
+    printf("Username: \t");
+    scanf("%s", &input.username);
+
+    fp = fopen("user.dat", "rb");
+    tmp = fopen("tmp.dat", "wb");
+
+    // Read all data from original file
+    while(
+          fread(&fromFile, sizeof(fromFile), 1, fp) == 1
+          )
+    {
+        // If the username of data from file matches
+        // We update the data and save it to temp file
+        if(strcmp(fromFile.username, input.username) == 0)
+        {
+            printf("Input New Password: \t");
+            scanf("%s", input.password);
+
+            fwrite(&input, sizeof(input), 1, tmp);
+        }
+        // We save the data from original file to temp file
+        else
+        {
+            fwrite(&fromFile, sizeof(fromFile), 1, tmp);
+        }
+    }
+    fclose(fp);
+    fclose(tmp);
+    remove("user.dat");
+    rename("tmp.dat", "user.dat");
+
+    printf("Data updated. Press any key to continue..");
+    getch();
 }
 
 void Menu()
@@ -16,18 +80,19 @@ void Menu()
     while(1)
     {
         system("cls");
-        printf("1. Add \n");
-        printf("2. Divide \n");
-        printf("1. Exit \n");
+        printf("1. View Users \n");
+        printf("2. Update User \n");
+        printf("3. Exit \n");
         printf("Please make your choice: ");
         scanf("%d", &choice);
 
         switch(choice)
         {
         case 1:
-            PerformAdd();
+            ViewUserList();
             break;
         case 2:
+            UpdateUser();
             break;
         case 3:
             exit(0);
@@ -74,13 +139,6 @@ void StartApp()
         }
     }
 }
-
-struct User
-{
-    char username[500];
-    char password[500];
-};
-
 int Login()
 {
     char username[500], password[500];
